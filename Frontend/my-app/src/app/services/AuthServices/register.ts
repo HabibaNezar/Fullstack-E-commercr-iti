d67a5905c29cdd6c5fdf11719a2f42ea1663548e
Esraa Shiref <israashiref@gmail.com>
2026-05-11 02:57:46 +0300

    Refactor AuthServices: Update service imports and change API URL to port 3001

--- Frontend/my-app/src/app/services/AuthServices/register.ts
+++ Frontend/my-app/src/app/services/AuthServices/register.ts
@@ -6,7 +6,7 @@ import { IUser } from '../../models/iuser';
 
 @Injectable({ providedIn: 'root' })
 export class RegisterService {
-  private apiUrl = 'http://localhost:3000/users';
+  private apiUrl = 'http://localhost:3001/users';
 
   constructor(private http: HttpClient) {}
 
