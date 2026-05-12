2cd0746633d044638f717cdb9f9ce67012c4400d
Eman Muhammed Genedy <genedyeman6@gmail.com>
2026-05-12 23:47:52 +0200

    Updated AppUser model, updated register/login logic, and fixed multiple cascade paths in DbContext.

--- Backend/LoginAndRegister/Models/AppUser.cs
+++ Backend/LoginAndRegister/Models/AppUser.cs
@@ -4,6 +4,8 @@ namespace LoginAndRegister.Models
 {
     public class AppUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string? ProfilePicture { get; set; } 
