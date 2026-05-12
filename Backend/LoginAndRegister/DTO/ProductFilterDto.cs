d60044319130b2f55ba2c930bdd4f96b81cfe3b9
Eman Muhammed Genedy <genedyeman6@gmail.com>
2026-05-13 01:36:21 +0200

    Add Admin & Seller Controller

---
+++ Backend/LoginAndRegister/DTO/ProductFilterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace LoginAndRegister.DTO
+{
+    public class ProductFilterDto
+    {
+        public string? Search {  get; set; }
+        public int? CategoryId { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public string? Sort { get; set; }
+    }
+}
