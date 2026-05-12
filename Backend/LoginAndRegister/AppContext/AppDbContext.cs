6c92a6dddb7c0f3cf58a6061fcd127fcbaf10c43
Eman Muhammed Genedy <genedyeman6@gmail.com>
2026-05-12 14:17:49 +0200

    Add BackEnd Logic (Account - Products - Category - Products - Orders - CheckOut)

---
+++ Backend/LoginAndRegister/AppContext/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using LoginAndRegister.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace LoginAndRegister.AppContext
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        // Connection With DB
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CartItems> CartItems { get; set; } 
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> orderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // هنا انا بقوله انا عندى جدول منتجات 
+            // الجدول دا ليه سيلر واحد بس وفيه كذا مستخدم ممكن يشترى منه
+            // فلو مسحنا يوزر وفى نفس الوقت اليوزر دا كان سيلر متمسحش كل المنتجات بتاعته
+            base.OnModelCreating(builder);
+            builder.Entity<Product>()
+                   .HasOne(p => p.Seller)
+                   .WithMany() 
+                   .HasForeignKey(p => p.SellerId)
+                   // فى السطر دا بقوله متعملش مسح تلقائى
+                   .OnDelete(DeleteBehavior.Cascade); 
+        }
+    }
+}
---
+++ Backend/LoginAndRegister/DTO/DtoNewUser.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginAndRegister.DTO
+{
+    public class DtoNewUser
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string City { get; set; }
+        public string Role { get; set; }
+    }
+}
