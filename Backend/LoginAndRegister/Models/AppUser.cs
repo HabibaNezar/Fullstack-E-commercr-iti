b89aa3a5485ca782dadad92ccbef436857925ac7
Eman Muhammed Genedy <genedyeman6@gmail.com>
2026-05-12 14:17:49 +0200

    Add BackEnd Logic (Account - Products - Category - Products - Orders - CheckOut)

---
+++ Backend/LoginAndRegister/Models/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LoginAndRegister.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string? ProfilePicture { get; set; } 
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        // Relation With CartItem
+        public virtual ICollection<CartItems> CartItems{ get; set; } = new HashSet<CartItems>();   
+    }
+}
