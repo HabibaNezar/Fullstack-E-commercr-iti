90ae1a1c02c7ec0caa0e528543da6ef1ee34aa0d
Esraa Shiref <israashiref@gmail.com>
2026-05-14 14:32:55 +0300

    fix: stop tracking Backend folder

--- Backend/LoginAndRegister/Services/IEmailSender.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LoginAndRegister.Services
-{
-    public interface IEmailSender
-    {
-        // أي حد عايز يبعت إيميل لازم يكون عنده ميثود اسمها SendEmailAsync
-        Task SendEmailAsync(string email, string subject, string message);
-        // SMTP : Simple Mail Transfer Protocol
-    }
-}
