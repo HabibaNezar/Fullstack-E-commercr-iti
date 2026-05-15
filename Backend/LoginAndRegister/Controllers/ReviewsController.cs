625223e23dbdeb0b70187c1e09a6ab25a4bc0acf
Eman Muhammed Genedy <genedyeman6@gmail.com>
2026-05-15 14:59:49 +0200

    Adding Review Table

---
+++ Backend/LoginAndRegister/Controllers/ReviewsController.cs
@@ -0,0 +1,24 @@
+﻿using LoginAndRegister.AppContext;
+using LoginAndRegister.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginAndRegister.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
+        public ReviewsController(AppDbContext context , UserManager<AppUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+    }
+}
---
+++ Backend/LoginAndRegister/Models/Review.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginAndRegister.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        [Range(1,5)]
+        public int Rating { get; set; }
+        public string Comment  { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        // Relation with Product
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        // Relation With User
+        public string UserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
